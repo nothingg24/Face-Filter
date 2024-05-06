7eeac9552df5094008d01343e44ada754a99fe6e
nothingg <trando2002@gmail.com>
2024-05-06 20:48:02 +0700

    .

--- notebooks/kalman.py
+++ notebooks/kalman.py
@@ -5,7 +5,9 @@ import math
 class KalmanFilter: #(cv2.KalmanFilter)
     def __init__(self, point) -> None:
         self.point = point
-        self.kalman = cv2.KalmanFilter(4, 2, 0, cv2.CV_64F)
+        self.stateNum = 4
+        self.measureNum = 2
+        self.kalman = cv2.KalmanFilter(self.stateNum, self.measureNum, 0, cv2.CV_64F)
         self.is_predicted = False
         self.deltaTime = 0.2 #0.2/1/0.5/0.7
         self.accelNoiseMag = 0.3 #0.3/1/0.7/1
