06a388dba07ea54837dc690821f327f18498cf2f
nothingg <trando2002@gmail.com>
2024-05-07 21:24:21 +0700

    ,

--- notebooks/mediapipe_detect.py
+++ notebooks/mediapipe_detect.py
@@ -17,6 +17,8 @@ def detect(option: Optional[str] = None):
         out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
     
     isFirstFrame = True
+    detector = mp.solutions.face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
+    face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, refine_landmarks=False, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
     while capture.isOpened():
         ret, frame = capture.read()
@@ -31,12 +33,10 @@ def detect(option: Optional[str] = None):
             fps = str(fps)
             cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
             
-            detector = mp.solutions.face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
             faces = detector.process(img_frame).detections
-            detector.close()
+            # detector.close()
 
             landmark_points = []
-            face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, refine_landmarks=False, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
             if faces is not None:
                 for face in faces:
@@ -44,9 +44,20 @@ def detect(option: Optional[str] = None):
                     bbox = face.location_data.relative_bounding_box
                     ih, iw, _ = img_frame.shape
                     x, y, w, h = int(bbox.xmin * iw), int(bbox.ymin * ih), int(bbox.width * iw), int(bbox.height * ih)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    top_left = (x, y)
+                    bottom_right = (x + w, y + h)
+                    bbox_points = [top_left, bottom_right]
+                    trackBbox = []
+                    if len(trackBbox) == 0:
+                        trackBbox = [KalmanFilter(point) for point in bbox_points]
+                    else:
+                        KalmanFilter.trackpoints(img2GrayPrev, img2Gray, bbox_points, trackBbox)
+                    
+                    cv2.rectangle(frame, tuple(map(int, trackBbox[0].getPoint())), tuple(map(int, trackBbox[1].getPoint())), (0, 255, 0), 2)
+                    # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                    face_cropped = img_frame[y:y+h, x:x+w]
+                    # face_cropped = img_frame[y:y+h, x:x+w]
+                    face_cropped = img_frame[int(trackBbox[0].getPoint()[1]):int(trackBbox[1].getPoint()[1]), int(trackBbox[0].getPoint()[0]):int(trackBbox[1].getPoint()[0])]
 
                     landmark_points_cropped = face_mesh.process(cv2.cvtColor(face_cropped, cv2.COLOR_BGR2RGB))
                     multi_face_landmarks = landmark_points_cropped.multi_face_landmarks
@@ -60,31 +71,27 @@ def detect(option: Optional[str] = None):
                                 face_landmarks_points.append((x_origin, y_origin))
 
                     landmark_points.append(face_landmarks_points)
-                    print(landmark_points)
 
-            face_mesh.close()
+                    points2 = landmark_points[0]
+                    trackPoints = []
+                    img2Gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            if len(landmark_points) > 0:
-                points2 = landmark_points[0]
-                trackPoints = []
-                img2Gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    if isFirstFrame:
+                        img2GrayPrev = np.copy(img2Gray)
+                        isFirstFrame = False
 
-                if isFirstFrame:
-                    img2GrayPrev = np.copy(img2Gray)
-                    isFirstFrame = False
+                    if len(trackPoints) == 0:
+                        trackPoints = [KalmanFilter(point) for point in points2]
+                    else:
+                        KalmanFilter.trackpoints(img2GrayPrev, img2Gray, points2, trackPoints)
 
-                if len(trackPoints) == 0:
-                    trackPoints = [KalmanFilter(point) for point in points2]
-                else:
-                    KalmanFilter.trackpoints(img2GrayPrev, img2Gray, points2, trackPoints)
+                    img2GrayPrev = img2Gray
 
-                img2GrayPrev = img2Gray
+                    for tp in trackPoints:
+                        cv2.circle(frame, tuple(map(int, tp.getPoint())), 3, (0, 0, 255) if tp.isPredicted() else (0, 255, 0), cv2.FILLED)
+                    cv2.imshow('landmark', frame)
 
-                # for point in points2:
-                #     cv2.circle(frame, point, 2, (0, 255, 0), -1)
-                for tp in trackPoints:
-                    cv2.circle(frame, tuple(map(int, tp.getPoint())), 3, (0, 0, 255) if tp.isPredicted() else (0, 255, 0), cv2.FILLED)
-            cv2.imshow('landmark', frame)
+            # face_mesh.close()
 
             if option != '0':
                 out.write(frame)
